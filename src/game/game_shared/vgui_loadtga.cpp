681d8d2b1f253705f229ab880d686c11d7ecd50a
Solokiller <duo929@hotmail.com>
2021-04-05 21:47:20 +0200

    Restructure existing files

--- src/game/game_shared/vgui_loadtga.cpp
+++ src/game/game_shared/vgui_loadtga.cpp
@@ -5,8 +5,8 @@
 // $NoKeywords: $
 //=============================================================================
 
-#include "../cl_dll/wrect.h"
-#include "../cl_dll/cl_dll.h"
+#include "../client/wrect.h"
+#include "../client/cl_dll.h"
 #include "VGUI.h"
 #include "vgui_loadtga.h"
 #include "VGUI_InputStream.h"
