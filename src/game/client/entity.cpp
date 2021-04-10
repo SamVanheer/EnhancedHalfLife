7f617d3298dd2209e1c4bd412cf32b5e30126e48
Solokiller <duo929@hotmail.com>
2021-04-10 20:05:29 +0200

    Use Vector instead of float[3] #20

--- src/game/client/entity.cpp
+++ src/game/client/entity.cpp
@@ -269,7 +269,7 @@ void Beams()
 	BeamEndModel();
 	
 	curtime = gEngfuncs.GetClientTime();
-	float end[ 3 ];
+	Vector end;
 
 	if ( ( curtime - lasttime ) < 10.0 )
 		return;
--- src/public/con_nprint.h
+++ src/public/con_nprint.h
@@ -19,7 +19,7 @@ struct con_nprint_t
 {
 	int		index;			// Row #
 	float	time_to_live;	// # of seconds before it dissappears
-	float	color[ 3 ];		// RGB colors ( 0.0 -> 1.0 scale )
+	Vector	color;			// RGB colors ( 0.0 -> 1.0 scale )
 };
 
 void Con_NPrintf( int idx, char *fmt, ... );
