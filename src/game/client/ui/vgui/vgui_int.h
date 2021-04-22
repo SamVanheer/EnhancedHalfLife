3efebee1377b685378800bab76b5272f9ae11609
Solokiller <duo929@hotmail.com>
2021-04-22 14:14:29 +0200

    Remove unnecessary extern "C" in vgui code Resolves #27

--- src/game/client/ui/vgui/vgui_int.h
+++ src/game/client/ui/vgui/vgui_int.h
@@ -7,12 +7,8 @@
 
 #pragma once
 
-//TODO: remove extern "C"
-extern "C"
-{
-	void VGui_Startup();
-	void VGui_Shutdown();
+void VGui_Startup();
+void VGui_Shutdown();
 
-	//Only safe to call from inside subclass of Panel::paintBackground
-	void VGui_ViewportPaintBackground(int extents[4]);
-}
+//Only safe to call from inside subclass of Panel::paintBackground
+void VGui_ViewportPaintBackground(int extents[4]);
