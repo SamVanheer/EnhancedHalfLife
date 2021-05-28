e9d5b1f01f1d5bb71efed2c45a67da6ee3b02c3e
Solokiller <duo929@hotmail.com>
2021-05-28 15:42:25 +0200

    Move code from subs.cpp to their respective class source files #6 #78

---
+++ src/game/server/entities/point/CPointEntity.cpp
@@ -0,0 +1,22 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+
+#include "CPointEntity.hpp"
+
+void CPointEntity::Spawn()
+{
+	SetSolidType(Solid::Not);
+	//	SetSize( vec3_origin, vec3_origin);
+}
